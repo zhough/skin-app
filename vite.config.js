9f5dcd34decde62ce07a6c6a908b4596da0c297c
zhough <453854697@qq.com>
2025-11-05 20:30:45 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8000,         // 端口改为8000
-    host: '0.0.0.0',    // 允许外部访问（关键配置）
+    port: 8000,         // 端口8000
+    host: '0.0.0.0',    // 允许外部访问
     open: true,
-    // 可选：如果有跨域需求，可添加允许的源
     cors: {
-      origin: '*'       // 允许所有外部源访问（生产环境需限制具体域名）
+      origin: '*'       // 允许所有外部源访问
     }
   },
   resolve: {
