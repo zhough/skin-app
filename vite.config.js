8026b2c36ab872e669492d54f29f66cfa9c3bf68
zhough <453854697@qq.com>
2025-11-08 19:01:02 +0800

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [vue()],
   server: {
     port: 5002,
     host: '0.0.0.0',
