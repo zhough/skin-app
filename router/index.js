14c947f36a1d9fb70a5fa4218e5166b18e621b9d
zhough <453854697@qq.com>
2025-11-09 13:18:18 +0800

    modified:   router/index.js

--- router/index.js
+++ router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { isLoggedIn } from '../services/auth-service.js'
 
 // 定义路由
@@ -88,7 +88,7 @@ const routes = [
 
 // 创建路由实例
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
